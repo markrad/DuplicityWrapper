5ad03ec44c978c9af01780f0c79d1051b914ec7a
Mark Radbourne <markrad@msn.com>
2022-02-04 13:48:55 -0800

    :construction: Tweaking publish

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TimeSpanOption = exports.DuplicityWrapper = void 0;
+const src_1 = require("./output/src");
+Object.defineProperty(exports, "DuplicityWrapper", { enumerable: true, get: function () { return src_1.DuplicityWrapper; } });
+Object.defineProperty(exports, "TimeSpanOption", { enumerable: true, get: function () { return src_1.TimeSpanOption; } });
+module.exports = { DuplicityWrapper: src_1.DuplicityWrapper, TimeSpanOption: src_1.TimeSpanOption };
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { DuplicityWrapper, TimeSpanOption } from "./src";
+
+export { DuplicityWrapper, TimeSpanOption };
+
+module.exports = { DuplicityWrapper, TimeSpanOption };
